f8933ffd410062a3691ee5b97772414f264ea6a4
schasins <schasins@gmail.com>
2014-07-03 15:04:24 -0700

    Reversing injection order.

--- src/scripts/lib/record-replay/content_script.js
+++ src/scripts/lib/record-replay/content_script.js
@@ -776,5 +776,5 @@ function injectScripts(paths) {
 
 // TODO(sbarman): need to wrap these so variables don't escape into the
 // enclosing scope
-injectScripts(["scripts/lib/record-replay/content_injected.js",
-				"scripts/lib/record-replay/common_params.js"]);
+injectScripts(["scripts/lib/record-replay/common_params.js", 
+               "scripts/lib/record-replay/content_injected.js"]);
